48952fa139d6f8e080be0072454cca8fb5035159
thelittlecormorant <TheLittleCormorant@users.noreply.github.com>
2021-01-19 05:09:55 +0530

    resume upload changes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  // private final String ONE_PAGE_PATH = "../assets/AnbuOnePage.pdf";
+  // private final String FULL_RESUME_PATH = "../assets/AnbuOnePage.pdf";
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onPress1Page(){
+    window.open('../assets/AnbuOnePage.pdf', '_blank');
+  }
+
+  onPressFullPage(){
+    window.open('../assets/AnbuFull.pdf', '_blank');
   }
 
 }
